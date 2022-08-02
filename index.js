ffa310fa9be7687b48d90d86d10a360e3719430d
Evans Ochieng <evansochieng121@gmail.com>
2022-08-02 18:29:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+// Add function
+function add(a, b){
+    return a + b
+}
+
+// Subtract function
+function subtract(a, b){
+    return a - b
+}
+
+// Multiply function
+function multiply(a, b){
+    return a * b
+}
+
+// Divide function
+function divide(a, b){
+    return a / b
+}
+
+// Increment function
+function increment(n){
+    return n += 1
+}
+
+// Decrement function
+function decrement(n){
+    return n -= 1
+}
+
+// Parsing numbers (ParseInt())
+function makeInt(string){
+    return parseInt(string, 10)
+}
+
+// Parsing numbers (ParseFloat())
+function preserveDecimal(string){
+    return parseFloat(string)
+}
